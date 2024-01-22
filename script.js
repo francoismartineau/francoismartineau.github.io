4468ab242e771c235421c092afa90b76f58353b5
François Martineau <ffrancoismmartineau@gmail.com>
2024-01-22 11:39:59 -0500

    display, prop, internship

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ fetch('projects.json')
 .then(response => response.json())
 .then(data => {
   const container = document.getElementById('projectsContainer');
+  const row = document.createElement('div');
+  row.classList.add('row');
   data.forEach(project => {
     const div = document.createElement('div');
+    div.classList.add('col-md-4');
     div.innerHTML = 
     `<div class="card" style="width: 18rem;">
       <img src="${project.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${project.title}</h5>
         <p class="card-text">${project.text}</p>
-        <a href="${project.link}" class="btn btn-primary">Repository</a>
+        <a href="${project.link}" class="btn btn-primary">More</a>
       </div>
     </div>`;    
-    container.appendChild(div);
+    row.appendChild(div);
   });
+  container.appendChild(row);
 })
 .catch(error => console.error('Error:', error));
